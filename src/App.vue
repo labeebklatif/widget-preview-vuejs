9e91fff719756987f6c71d71cd9e0ec432d4571e
Labeeb Latheef <labeebklatif@gmail.com>
2022-06-16 12:39:17 +0530

    widget install to mount

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
 <script setup>
+import { onMounted } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+
+onMounted(() => {
+  (function () {
+    var w = window,
+      d = document,
+      z = w.NeetoChat || {};
+    z.subdomain = "spinkart";
+    z.chatApiKey = "kWJCAyH4JbWF3Opx";
+    z.neetoChatBaseUrl = "https://spinkart.neetochat.net";
+    z.neetoKbBaseUrl = "https://spinkart.neetokb.net";
+    z.neetoDeskBaseUrl = "https://spinkart.neetodesk.net";
+    w.NeetoChat = z;
+    var inserted = false;
+    function insertTag() {
+      if (inserted) return;
+      inserted = true;
+      var s = d.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src =
+        "https://neeto-chat-widget-production-v1.s3.amazonaws.com/bundle-loader.js";
+      var x = d.getElementsByTagName("script")[0];
+      x.parentNode.insertBefore(s, x);
+    }
+    if (document.readyState != "loading") {
+      insertTag();
+    } else {
+      document.onreadystatechange = function () {
+        if (document.readyState != "loading") insertTag();
+      };
+    }
+  })();
+});
 </script>
 
 <template>
